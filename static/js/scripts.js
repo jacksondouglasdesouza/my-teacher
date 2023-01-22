061b9345826caf1afeed8d7757e393c8000e6766
jacksondouglasdesouza <jacksondouglasdesouza@hotmail.com>
2023-01-22 18:55:35 -0300

    Deploy: Execution of the Implementation. { I performed the first project commit, with all its functionalities tested. }

---
+++ static/js/scripts.js
@@ -0,0 +1,252 @@
+function prof001(){
+    event.preventDefault();
+
+    let price = document.getElementById("price").value;
+
+    let hours = document.getElementById("hours").value;
+
+    let total = price * hours;
+
+    if(hours > 0 && hours <= 8){
+        document.getElementById("pag-01").innerHTML = "Você vai pagar: R$ " + total;
+    }
+    else
+    {
+        document.getElementById("pag-01").innerHTML = "Insira valor entre 1 e 8";
+    }
+
+}
+
+function prof002(){
+    event.preventDefault();
+
+    let price = document.getElementById("price002").value;
+
+    let hours = document.getElementById("hours002").value;
+
+    let total = price * hours;
+
+    if(hours > 0 && hours <= 8){
+        document.getElementById("pag-002").innerHTML = "Você vai pagar: R$ " + total;
+    }
+    else
+    {
+        document.getElementById("pag-002").innerHTML = "Insira valor entre 1 e 8";
+    }
+
+}
+
+
+function prof003(){
+    event.preventDefault();
+
+    let price = document.getElementById("price003").value;
+
+    let hours = document.getElementById("hours003").value;
+
+    let total = price * hours;
+
+    if(hours > 0 && hours <= 8){
+        document.getElementById("pag-003").innerHTML = "Você vai pagar: R$ " + total;
+    }
+    else
+    {
+        document.getElementById("pag-003").innerHTML = "Insira valor entre 1 e 8";
+    }
+
+}
+
+function prof004(){
+    event.preventDefault();
+
+    let price = document.getElementById("price004").value;
+
+    let hours = document.getElementById("hours004").value;
+
+    let total = price * hours;
+
+    if(hours > 0 && hours <= 8){
+        document.getElementById("pag-004").innerHTML = "Você vai pagar: R$ " + total;
+    }
+    else
+    {
+        document.getElementById("pag-004").innerHTML = "Insira valor entre 1 e 8";
+    }
+
+}
+
+function prof005(){
+    event.preventDefault();
+
+    let price = document.getElementById("price005").value;
+
+    let hours = document.getElementById("hours005").value;
+
+    let total = price * hours;
+
+    if(hours > 0 && hours <= 8){
+        document.getElementById("pag-005").innerHTML = "Você vai pagar: R$ " + total;
+    }
+    else
+    {
+        document.getElementById("pag-005").innerHTML = "Insira valor entre 1 e 8";
+    }
+
+}
+
+function prof006(){
+    event.preventDefault();
+
+    let price = document.getElementById("price006").value;
+
+    let hours = document.getElementById("hours006").value;
+
+    let total = price * hours;
+
+    if(hours > 0 && hours <= 8){
+        document.getElementById("pag-006").innerHTML = "Você vai pagar: R$ " + total;
+    }
+    else
+    {
+        document.getElementById("pag-006").innerHTML = "Insira valor entre 1 e 8";
+    }
+
+}
+
+
+function prof007(){
+    event.preventDefault();
+
+    let price = document.getElementById("price007").value;
+
+    let hours = document.getElementById("hours007").value;
+
+    let total = price * hours;
+
+    if(hours > 0 && hours <= 8){
+        document.getElementById("pag-007").innerHTML = "Você vai pagar: R$ " + total;
+    }
+    else
+    {
+        document.getElementById("pag-007").innerHTML = "Insira valor entre 1 e 8";
+    }
+
+}
+
+
+function prof008(){
+    event.preventDefault();
+
+    let price = document.getElementById("price008").value;
+
+    let hours = document.getElementById("hours008").value;
+
+    let total = price * hours;
+
+    if(hours > 0 && hours <= 8){
+        document.getElementById("pag-008").innerHTML = "Você vai pagar: R$ " + total;
+    }
+    else
+    {
+        document.getElementById("pag-008").innerHTML = "Insira valor entre 1 e 8";
+    }
+
+}
+
+
+function prof009(){
+    event.preventDefault();
+
+    let price = document.getElementById("price009").value;
+
+    let hours = document.getElementById("hours009").value;
+
+    let total = price * hours;
+
+    if(hours > 0 && hours <= 8){
+        document.getElementById("pag-009").innerHTML = "Você vai pagar: R$ " + total;
+    }
+    else
+    {
+        document.getElementById("pag-009").innerHTML = "Insira valor entre 1 e 8";
+    }
+
+}
+
+
+function prof010(){
+    event.preventDefault();
+
+    let price = document.getElementById("price010").value;
+
+    let hours = document.getElementById("hours010").value;
+
+    let total = price * hours;
+
+    if(hours > 0 && hours <= 8){
+        document.getElementById("pag-010").innerHTML = "Você vai pagar: R$ " + total;
+    }
+    else
+    {
+        document.getElementById("pag-010").innerHTML = "Insira valor entre 1 e 8";
+    }
+
+}
+
+
+function prof011(){
+    event.preventDefault();
+
+    let price = document.getElementById("price011").value;
+
+    let hours = document.getElementById("hours011").value;
+
+    let total = price * hours;
+
+    if(hours > 0 && hours <= 8){
+        document.getElementById("pag-011").innerHTML = "Você vai pagar: R$ " + total.toFixed(2);
+    }
+    else
+    {
+        document.getElementById("pag-011").innerHTML = "Insira valor entre 1 e 8";
+    }
+
+}
+
+function prof012(){
+    event.preventDefault();
+
+    let price = document.getElementById("price012").value;
+
+    let hours = document.getElementById("hours012").value;
+
+    let total = price * hours;
+
+    if(hours > 0 && hours <= 8){
+        document.getElementById("pag-012").innerHTML = "Você vai pagar: R$ " + total.toFixed(2);
+    }
+    else
+    {
+        document.getElementById("pag-012").innerHTML = "Insira valor entre 1 e 8";
+    }
+
+}
+
+
+/* let teacherForms = document.getElementsByClassName("teacher-contract");
+
+for( let teacherForm of teacherForms){
+    teacherForm.addEventListener('submit',
+        function( event )
+        {
+            event.preventDefault();
+
+            let price = teacherForm.price.value;
+            let hours = teacherForm.hours.value;
+            let total = price * hours;
+
+            alert("O valor total é: " + total);
+
+        }
+    )
+} */
